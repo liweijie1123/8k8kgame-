f5bb87df7a96d3a726039e6043f4a07e33e37e97
Liweijie <1771662778@qq.com>
2022-05-14 21:46:19 +0800

    new

--- js/main.js
+++ js/main.js
@@ -1,31 +1,5 @@
 
 
-$(window).scroll(function () {
-
-    if ($(this).scrollTop() > $('nav').height()) {
-        $("nav").animate({
-            height:'100px'
-          });
-        $("nav").removeClass("navbar-dark")
-        $("nav").removeClass("balck")
-
-        $("nav").addClass("navbar-light")
-        $("nav").addClass("border")
-
-    } else {
-        $("nav").animate({
-            height:'110px'
-          });
-        $("nav").addClass("navbar-dark")
-        $("nav").addClass("balck")
-
-        $("nav").removeClass("navbar-light")
-        $("nav").removeClass("border")
-
-    }
-
-});
-
 
 $.getJSON("data.json", function(data) {
     var someGame = template('someGame', data);
@@ -37,6 +11,4 @@ $.getJSON("data.json", function(data) {
     box2.innerHTML=allGame;
 });
 
-var a = document.querySelector('iframe').src
-alert(a)
 
