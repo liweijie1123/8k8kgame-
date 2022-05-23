2c2c6ecb33fdd5456ee4497ce2ec23c82deedfb4
Liweijie <1771662778@qq.com>
2022-05-23 21:25:40 +0800

    完善搜索功能

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 let getEle = (e, all = false) => { return (all) ? document.querySelectorAll(`${e}`) : document.querySelector(`${e}`) }
 let addClEvt = (el, cb) => { el.addEventListener('click', cb) }
+let addKeyEvt = (cb)=>{addEventListener('keydown',cb)}
 // 模糊搜索
 let searcher = () => {
     $.getJSON("data.json", function (data) {
@@ -27,16 +28,34 @@ let load = () => {
 
     });
 }
-let reload  =()=>{
+let reload = () => {
     location.reload();
+}
+let allgame = () => {
+    box.style.display = 'none';
+    $.getJSON("data.json", function (data) {
+        var allGame = template('allGame', data);
+        box2.innerHTML = allGame;
+    });
+}
+let keydown = (e)=>{
+    if(e.keyCode==13){
+        searcher();
+    }
 }
 var box = getEle(".m-some_game");
 var box2 = getEle(".m-all_game")
 var search = getEle(".m-search-button");
 var input = getEle(".in");
 var logo = getEle(".m-logo");
+var meun = getEle(".m-meun");
+var more = getEle(".more");
 
 load();
 addClEvt(search, searcher);
-addClEvt(logo,reload);
+addClEvt(logo, reload);
+addClEvt(meun, allgame);
+addKeyEvt(keydown);
+setTimeout(addClEvt(more,reload),1000);
+
 
